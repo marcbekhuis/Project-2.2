a19907b9d0a3f0e64f93e539636c1a0926224075
Marc <marc@bekhuis.net>
2019-11-13 10:56:09 +0100

    Skylander Puzzle

---
+++ Project 2.2/Assets/Scripts/SkyLandersPuzzle/SkylanderPuzzleBoard.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkylanderPuzzleBoard : MonoBehaviour
+{
+    public char[,] board;
+    public RectTransform[,] boardGameObjects;
+    public RectTransform placedPlayer;
+    public Dictionary<RectTransform, Vector2Int> movables = new Dictionary<RectTransform, Vector2Int>();
+    [Space]
+    [SerializeField] private string[] inputBoard;
+    [SerializeField] private GameObject wall;
+    [SerializeField] private GameObject floor;
+    [SerializeField] private GameObject end;
+    [SerializeField] private GameObject player;
+    [Space]
+    [SerializeField] private RectTransform boardParent;
+    [SerializeField] private RectTransform playerParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        board = new char[inputBoard.Length, inputBoard[0].Length];
+        boardGameObjects = new RectTransform[inputBoard.Length, inputBoard[0].Length];
+        playerParent.sizeDelta = new Vector2(5 + 100 * board.GetLength(1) + 5 * board.GetLength(1), 5 + 100 * board.GetLength(0) + 5 * board.GetLength(0));
+        if ((board.GetLength(1) - 3) > (board.GetLength(0) - 3))
+        {
+            float temp = Mathf.Clamp((float)1 - ((float)(board.GetLength(1) - 3) * 0.1f),0,1);
+            playerParent.localScale = new Vector3(temp, temp, 1);
+        }
+        else
+        {
+            float temp = Mathf.Clamp((float)1 - ((float)(board.GetLength(0) - 3) * 0.1f), 0, 1);
+            playerParent.localScale = new Vector3(temp, temp, 1);
+        }
+
+        for (int x = 0; x < board.GetLength(0); x++)
+        {
+            for (int y = 0; y < board.GetLength(1); y++)
+            {
+                board[x, y] = inputBoard[x][y];
+                switch (board[x,y])
+                {
+                    case '_':
+                        boardGameObjects[x,y] = Instantiate(floor, new Vector3(0,0,0), new Quaternion(0,0,0,0),boardParent).GetComponent<RectTransform>();
+                        boardGameObjects[x, y].anchoredPosition = new Vector2(5 + 100 * y + 5 * y, -5 - 100 * x - 5 * x);
+                        break;
+                    case 'O':
+                        boardGameObjects[x, y] = Instantiate(floor, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0), boardParent).GetComponent<RectTransform>();
+                        boardGameObjects[x, y].anchoredPosition = new Vector2(5 + 100 * y + 5 * y, -5 - 100 * x - 5 * x);
+                        placedPlayer = Instantiate(player, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0), playerParent).GetComponent<RectTransform>();
+                        placedPlayer.anchoredPosition = boardGameObjects[x, y].anchoredPosition;
+                        movables.Add(placedPlayer,new Vector2Int(x,y));
+                        break;
+                    case 'X':
+                        boardGameObjects[x, y] = Instantiate(end, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0), boardParent).GetComponent<RectTransform>();
+                        boardGameObjects[x, y].anchoredPosition = new Vector2(5 + 100 * y + 5 * y, -5 - 100 * x - 5 * x);
+                        break;
+                    case '#':
+                        boardGameObjects[x, y] = Instantiate(wall, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0), boardParent).GetComponent<RectTransform>();
+                        boardGameObjects[x, y].anchoredPosition = new Vector2(5 + 100 * y + 5 * y, -5 - 100 * x - 5 * x);
+                        break;
+                    default:
+                        Debug.Log("Something Broke");
+                        break;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Project 2.2/Assets/Scripts/SkyLandersPuzzle/SkylanderPuzzleMovement.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SkylanderPuzzleMovement : MonoBehaviour
+{
+    [SerializeField] private Transform board;
+    [SerializeField] private Rotations rotation = Rotations.Bottom;
+
+    private SkylanderPuzzleBoard skylanderPuzzleBoard;
+
+    private enum Rotations
+    {
+        top,
+        Left,
+        Bottom,
+        Right
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skylanderPuzzleBoard = GetComponent<SkylanderPuzzleBoard>();
+
+        rotation = Rotations.Bottom;
+        Physics();
+    }
+
+    public void RotateBoardLeft()
+    {
+        board.Rotate(new Vector3(0, 0, 90));
+        switch (rotation)
+        {
+            case Rotations.Bottom:
+                rotation = Rotations.Left;
+                Physics();
+                break;
+            case Rotations.Left:
+                rotation = Rotations.top;
+                Physics();
+                break;
+            case Rotations.Right:
+                rotation = Rotations.Bottom;
+                Physics();
+                break;
+            case Rotations.top:
+                rotation = Rotations.Right;
+                Physics();
+                break;
+        }
+    }
+
+    public void RotateBoardRight()
+    {
+        board.Rotate(new Vector3(0, 0, -90));
+        switch (rotation)
+        {
+            case Rotations.Bottom:
+                rotation = Rotations.Right;
+                Physics();
+                break;
+            case Rotations.Left:
+                rotation = Rotations.Bottom;
+                Physics();
+                break;
+            case Rotations.Right:
+                rotation = Rotations.top;
+                Physics();
+                break;
+            case Rotations.top:
+                rotation = Rotations.Left;
+                Physics();
+                break;
+        }
+    }
+
+    private void Physics()
+    {
+        foreach (var movingObject in skylanderPuzzleBoard.movables.ToList())
+        {
+            switch (rotation)
+            {
+                case Rotations.Bottom:
+                    for (int x = movingObject.Value.x + 1; x < skylanderPuzzleBoard.board.GetLength(0); x++)
+                    {
+                        if (skylanderPuzzleBoard.board[x, movingObject.Value.y] == '#')
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            skylanderPuzzleBoard.board[movingObject.Value.x, movingObject.Value.y] = '_';
+                            skylanderPuzzleBoard.movables[movingObject.Key] = new Vector2Int(x, movingObject.Value.y);
+                            movingObject.Key.anchoredPosition = skylanderPuzzleBoard.boardGameObjects[x, movingObject.Value.y].anchoredPosition;
+                            WinCheck(x, movingObject.Value.y);
+                        }
+                    }
+                    break;
+                case Rotations.Left:
+                    for (int y = movingObject.Value.y - 1; y >= 0; y--)
+                    {
+                        if (skylanderPuzzleBoard.board[movingObject.Value.x, y] == '#')
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            skylanderPuzzleBoard.board[movingObject.Value.x, movingObject.Value.y] = '_';
+                            skylanderPuzzleBoard.movables[movingObject.Key] = new Vector2Int(movingObject.Value.x, y);
+                            movingObject.Key.anchoredPosition = skylanderPuzzleBoard.boardGameObjects[movingObject.Value.x, y].anchoredPosition;
+                            WinCheck(movingObject.Value.x, y);
+                        }
+                    }
+                    break;
+                case Rotations.Right:
+                    for (int y = movingObject.Value.y + 1; y < skylanderPuzzleBoard.board.GetLength(1); y++)
+                    {
+                        if (skylanderPuzzleBoard.board[movingObject.Value.x, y] == '#')
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            skylanderPuzzleBoard.board[movingObject.Value.x, movingObject.Value.y] = '_';
+                            skylanderPuzzleBoard.movables[movingObject.Key] = new Vector2Int(movingObject.Value.x, y);
+                            movingObject.Key.anchoredPosition = skylanderPuzzleBoard.boardGameObjects[movingObject.Value.x, y].anchoredPosition;
+                            WinCheck(movingObject.Value.x, y);
+                        }
+                    }
+                    break;
+                case Rotations.top:
+                    for (int x = movingObject.Value.x - 1; x >= 0; x--)
+                    {
+                        if (skylanderPuzzleBoard.board[x, movingObject.Value.y] == '#')
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            skylanderPuzzleBoard.board[movingObject.Value.x, movingObject.Value.y] = '_';
+                            skylanderPuzzleBoard.movables[movingObject.Key] = new Vector2Int(x, movingObject.Value.y);
+                            movingObject.Key.anchoredPosition = skylanderPuzzleBoard.boardGameObjects[x, movingObject.Value.y].anchoredPosition;
+                            WinCheck(x, movingObject.Value.y);
+                        }
+                    }
+                    break;
+            }
+            
+        }
+    }
+
+    private void WinCheck(int x, int y)
+    {
+        if(skylanderPuzzleBoard.board[x, y] == 'X')
+        {
+            Debug.Log("You Won");
+        }
+        else
+        {
+            skylanderPuzzleBoard.board[x, y] = 'O';
+        }
+    }
+}
