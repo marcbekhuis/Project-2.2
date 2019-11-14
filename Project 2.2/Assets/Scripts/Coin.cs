a022b23a67ab9acd126ed9bc02566b7fc2752dea
helpower2 <jeffreymonnink@gmail.com>
2019-11-14 14:34:24 +0100

    fixed coins

--- Project 2.2/Assets/Scripts/Coin.cs
+++ Project 2.2/Assets/Scripts/Coin.cs
@@ -6,18 +6,11 @@ public class Coin : MonoBehaviour
 {
     [SerializeField] int points = 1;
 
-    private LevelScore levelScore;
-
-    private void Start()
-    {
-        levelScore = FindObjectOfType<LevelScore>();
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            levelScore.AddScore(points);
+            LevelScore.Instance.AddScore(points);
             Destroy(this.gameObject);
         }
     }
