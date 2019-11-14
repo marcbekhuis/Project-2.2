d8a941c7a3419cbd313f898f9a27cf8d36d1f892
marcbekhuis <marc@bekhuis.net>
2019-11-14 13:50:04 +0100

    Score

---
+++ Project 2.2/Assets/Scripts/Coin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] int points = 1;
+
+    private LevelScore levelScore;
+
+    private void Start()
+    {
+        levelScore = FindObjectOfType<LevelScore>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            levelScore.AddScore(points);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Project 2.2/Assets/Scripts/LevelScore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelScore : MonoBehaviour
+{
+    [SerializeField] private Text scoreText;
+    private int score = 0;
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
